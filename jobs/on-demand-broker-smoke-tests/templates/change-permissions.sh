efc5046151e0e0f1461b0171efad70e2a2c537fb
Rosie Bloxsom <rbloxsom@pivotal.io>
2018-08-03 12:11:07 +0100

    Hardcode on-demand-broker-smoke-tests directory in on-demand-broker-smoke-tests job
    [#158386738]

--- jobs/on-demand-broker-smoke-tests/templates/change-permissions.sh
+++ jobs/on-demand-broker-smoke-tests/templates/change-permissions.sh
@@ -3,8 +3,8 @@
 [ -z "$DEBUG" ] || set -x
 
 ensure_directories_are_not_world_readable() {
-  chmod 750 "/var/vcap/jobs/smoke-tests"
-  chown -LR vcap:vcap "/var/vcap/jobs/smoke-tests"
+  chmod 750 "/var/vcap/jobs/on-demand-broker-smoke-tests"
+  chown -LR vcap:vcap "/var/vcap/jobs/on-demand-broker-smoke-tests"
 }
 
 ensure_directories_are_not_world_readable
--- jobs/on-demand-broker-smoke-tests/templates/permissions-test.sh
+++ jobs/on-demand-broker-smoke-tests/templates/permissions-test.sh
@@ -9,14 +9,14 @@ fail() {
 
 test_directories_are_not_world_readable() {
   echo "Checking directory permissions"
-  directory_breaking_the_rules=$(find -L "/var/vcap/jobs/smoke-tests" -maxdepth 0 -perm /o+rwx -type d)
+  directory_breaking_the_rules=$(find -L "/var/vcap/jobs/on-demand-broker-smoke-tests" -maxdepth 0 -perm /o+rwx -type d)
   [[ -z "${directory_breaking_the_rules}" ]] ||
-  fail "the following directory is world readable: /var/vcap/jobs/smoke-tests"
+  fail "the following directory is world readable: /var/vcap/jobs/on-demand-broker-smoke-tests"
 
   echo "Checking directory ownership"
-  directory_owned_by_vcap=$(find -L "/var/vcap/jobs/smoke-tests" -maxdepth 0 -group vcap -user vcap -type d)
+  directory_owned_by_vcap=$(find -L "/var/vcap/jobs/on-demand-broker-smoke-tests" -maxdepth 0 -group vcap -user vcap -type d)
   [[ -n "${directory_owned_by_vcap}" ]] ||
-  fail "the following directory is not owned by vcap:vcap: /var/vcap/jobs/smoke-tests"
+  fail "the following directory is not owned by vcap:vcap: /var/vcap/jobs/on-demand-broker-smoke-tests"
 }
 
 test_directories_are_not_world_readable

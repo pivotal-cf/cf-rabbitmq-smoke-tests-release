5ad1661a94487d76ec77f9133facddab1b6f7a03
George Blue <gblue@pivotal.io>
2018-12-19 17:48:54 +0000

    Use Ginkgo from BOSH package
    [#162666098]

--- jobs/smoke-tests/templates/run.sh
+++ jobs/smoke-tests/templates/run.sh
@@ -6,7 +6,7 @@ set -e
 
 export GOROOT=$(readlink -nf /var/vcap/packages/cf-rabbitmq-smoke-tests-golang)
 export GOPATH=/var/vcap/packages/cf-rabbitmq-smoke-tests
-export PATH=/var/vcap/packages/cf-cli-6-linux/bin:$GOPATH/bin:$GOROOT/bin:$PATH
+export PATH=/var/vcap/packages/cf-cli-6-linux/bin:/var/vcap/packages/cf-rabbitmq-smoke-tests-ginkgo/bin:$GOROOT/bin:$PATH
 export PACKAGE_DIR=${GOPATH}/src/rabbitmq-smoke-tests
 
 export CONFIG_PATH=/var/vcap/jobs/smoke-tests/config.json
@@ -17,6 +17,5 @@ export SMOKE_TESTS_TIMEOUT=1h
 pushd ${PACKAGE_DIR}
   echo "Running multitenant smoke tests"
   # Disbale Go modules and cgo to avoid issue https://github.com/golang/go/issues/26988
-  CGO_ENABLED=0 GO111MODULE=off go install -v github.com/onsi/ginkgo/ginkgo
   CGO_ENABLED=0 GO111MODULE=off ginkgo -v --trace -randomizeSuites=true -randomizeAllSpecs=true -keepGoing=true --timeout="$SMOKE_TESTS_TIMEOUT" -failOnPending tests
 popd

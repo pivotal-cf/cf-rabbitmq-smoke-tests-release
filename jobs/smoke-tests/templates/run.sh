85df773bceb732e9aa8980612f455bcd38efde2a
Connor Rogers <23215294+coro@users.noreply.github.com>
2022-10-12 16:44:51 +0100

    Use v2 versions of ginkgo command params

--- jobs/smoke-tests/templates/run.sh
+++ jobs/smoke-tests/templates/run.sh
@@ -19,5 +19,5 @@ export SMOKE_TESTS_TIMEOUT=1h
 pushd ${PACKAGE_DIR}
   echo "Running multitenant smoke tests"
   # Disbale Go modules and cgo to avoid issue https://github.com/golang/go/issues/26988
-  CGO_ENABLED=0 GO111MODULE=off ginkgo -v --trace -randomizeSuites=true -randomizeAllSpecs=true -keepGoing=true --timeout="$SMOKE_TESTS_TIMEOUT" -failOnPending tests
+  CGO_ENABLED=0 GO111MODULE=off ginkgo -v --trace --randomize-suites --randomize-all --keep-going --timeout="$SMOKE_TESTS_TIMEOUT" --fail-on-pending tests
 popd

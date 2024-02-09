efc01899d035098c9d0801c1e93e69e261c4bde3
Mirah Gary <mgary@vmware.com>
2024-02-09 12:06:24 +0100

    GO111MODULE variable removed in go1.18, always mandatory.

--- jobs/smoke-tests/templates/run.sh
+++ jobs/smoke-tests/templates/run.sh
@@ -5,7 +5,6 @@ set -e
 . /var/vcap/jobs/smoke-tests/bin/permissions-test
 
 export GOROOT=$(readlink -nf /var/vcap/packages/cf-rabbitmq-smoke-tests-golang)
-export GOPATH=/var/vcap/packages/cf-rabbitmq-smoke-tests
 export PATH=/var/vcap/packages/cf-cli-7-linux/bin:$GOPATH/bin:$GOROOT/bin:$PATH
 export PACKAGE_DIR=${GOPATH}/src/rabbitmq-smoke-tests
 
@@ -19,5 +18,5 @@ export SMOKE_TESTS_TIMEOUT=1h
 pushd ${PACKAGE_DIR}
   echo "Running multitenant smoke tests"
   # Disbale Go modules and cgo to avoid issue https://github.com/golang/go/issues/26988
-  CGO_ENABLED=0 GO111MODULE=off ginkgo -v --trace --randomize-suites --randomize-all --keep-going --timeout="$SMOKE_TESTS_TIMEOUT" --fail-on-pending tests
+  CGO_ENABLED=0 ginkgo -v --trace --randomize-suites --randomize-all --keep-going --timeout="$SMOKE_TESTS_TIMEOUT" --fail-on-pending tests
 popd

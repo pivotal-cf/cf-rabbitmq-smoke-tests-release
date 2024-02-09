f6f98c5e5aa401a519aaa29b46ae3e84c9cb59de
Mirah Gary <mgary@vmware.com>
2024-02-09 14:14:14 +0100

    make GOPATH explicit.

--- jobs/on-demand-broker-smoke-tests/templates/run.sh
+++ jobs/on-demand-broker-smoke-tests/templates/run.sh
@@ -2,7 +2,8 @@
 set -ex
 
 export GOROOT=$(readlink -nf /var/vcap/packages/cf-rabbitmq-smoke-tests-golang)
-export PATH=/var/vcap/packages/cf-cli-7-linux/bin:/var/vcap/packages/cf-rabbitmq-smoke-tests/bin:$GOROOT/bin:$PATH
+export GOPATH=/var/vcap/packages/cf-rabbitmq-smoke-tests
+export PATH=/var/vcap/packages/cf-cli-7-linux/bin:$GOPATH/bin:$GOROOT/bin:$PATH
 
 export GOCACHE=$PWD/cache
 

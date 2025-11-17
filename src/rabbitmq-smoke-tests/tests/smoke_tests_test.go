e585680835d9762668a906aa6238e8e2d74db28b
Apurva Saraiya <apurva.saraiya@broadcom.com>
2025-11-17 13:24:00 +0530

    chalo fix karte hai

--- src/rabbitmq-smoke-tests/tests/smoke_tests_test.go
+++ src/rabbitmq-smoke-tests/tests/smoke_tests_test.go
@@ -36,17 +36,6 @@ var _ = Describe("Smoke tests", func() {
 
 				helper.CreateServiceKey(serviceName, serviceKeyName)
 
-				defer func() {
-					By("unbinding the app")
-					helper.UnbindService(appName, serviceName)
-					By("deleting the app")
-					helper.DeleteApp(appName)
-					By("deleting the service key")
-					helper.DeleteServiceKey(serviceName, serviceKeyName)
-					By("deleting the service instance")
-					helper.DeleteService(serviceName)
-				}()
-
 				By("pushing and binding an app")
 				appURL := helper.PushAndBindApp(appName, serviceName, appPath)
 
